f24131c75769399c727d6a60e18829f02889bc16
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 20:00:23 +0700

    Partial updates with PATCH

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import Body
 from fastapi.encoders import jsonable_encoder
 
 from typing import Annotated
+import pprint
 
 from models.item import Item
 
@@ -34,6 +35,7 @@ ITEMS = {
 async def read_item(id: Annotated[str, Path()]) -> Item:
   return ITEMS[id]
 
+"""
 @app.put("/items/{id}", response_model = Item, response_model_exclude_unset = True)
 async def update_item(
   id: Annotated[str, Path()],
@@ -42,3 +44,19 @@ async def update_item(
   dict_item: dict[str, str] = jsonable_encoder(item)
   ITEMS[id] = dict_item
   return dict_item
+"""
+
+# Partial updates with PATCH
+@app.put("/items/{id}", response_model = Item, response_model_exclude_unset = True)
+async def update_item(
+  id: Annotated[str, Path()],
+  item: Annotated[Item, Body(embed = True)]
+) -> Item:
+  dict_stored_item = ITEMS[id]
+  stored_item = Item(**dict_stored_item)
+
+  dict_updating_item = item.model_dump(exclude_unset = True)
+  updated_item = stored_item.model_copy(update = dict_updating_item)
+
+  ITEMS[id] = jsonable_encoder(updated_item)
+  return updated_item
